0c510430ae60c63e73a7de43b6cccee3de2dbb2b
Censuradho <gustavoleiteoliveira800@gmail.com>
2026-04-20 16:14:03 -0300

    refactor(Header): reorganize JSX structure for improved readability

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import { MobileMenu } from "./MobileMenu";
 import { LinkButton } from "@/components/LinkButton";
 import { generateWhatsAppLink } from "@/utils/generateWhatsAppLink";
+import { TopInfo } from "./TopInfo";
 
 const navigation = [
   { label: 'Marcas', href: '/#marcas' },
@@ -12,45 +13,48 @@ const navigation = [
 
 export function Header() {
   return (
-    <header className="border-y border-outline bg-background-glass backdrop-blur-sm sticky top-0 z-50 px-4">
-      <div className="container h-[43px] flex items-center justify-between">
-        <Link href="/" className="text-[21px] font-semibold">
+    <>
+      <TopInfo />
+      <header className="border-y border-outline bg-background-glass backdrop-blur-sm sticky top-0 z-50 px-4">
+        <div className="container h-[43px] flex items-center justify-between">
+          <Link href="/" className="text-[21px] font-semibold">
           Ni<span className="text-primary">Cell</span>
-        </Link>
+          </Link>
 
-        <nav className="hidden lg:block">
-          <ul className="flex items-center gap-6">
-            {navigation.map((item) => (
-              <li key={item.href}>
-                <Link href={item.href} className="text-sm">
-                  {item.label}
-                </Link>
-              </li>
-            ))}
-            <li>
-              <a
-                className="text-sm"
-                href={generateWhatsAppLink('Olá! Gostaria de saber mais sobre os serviços da NiCell.')} 
-                target="_blank" 
-                rel="noopener noreferrer"
-              >
+          <nav className="hidden lg:block">
+            <ul className="flex items-center gap-6">
+              {navigation.map((item) => (
+                <li key={item.href}>
+                  <Link href={item.href} className="text-sm">
+                    {item.label}
+                  </Link>
+                </li>
+              ))}
+              <li>
+                <a
+                  className="text-sm"
+                  href={generateWhatsAppLink('Olá! Gostaria de saber mais sobre os serviços da NiCell.')} 
+                  target="_blank" 
+                  rel="noopener noreferrer"
+                >
                 Contato
-              </a>
-            </li>
-          </ul>
-        </nav>
+                </a>
+              </li>
+            </ul>
+          </nav>
 
-        <div className="hidden lg:flex items-center gap-3">
-          <LinkButton href="/#orcamento" variant="text" size="sm">
+          <div className="hidden lg:flex items-center gap-3">
+            <LinkButton href="/#orcamento" variant="text" size="sm">
             Orçamento online
-          </LinkButton>
-          <AnchorButton href="/login"  size="sm">
+            </LinkButton>
+            <AnchorButton href="/login"  size="sm">
             Login
-          </AnchorButton>
-        </div>
+            </AnchorButton>
+          </div>
 
-        <MobileMenu navigation={navigation} />
-      </div>
-    </header>
+          <MobileMenu navigation={navigation} />
+        </div>
+      </header>
+    </>
   )
 }
