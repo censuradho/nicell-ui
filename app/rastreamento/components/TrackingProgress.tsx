22d4c84a05958f8eef7687aad67ceeb2ea299fd3
Censuradho <gustavoleiteoliveira800@gmail.com>
2026-05-05 22:40:40 -0300

    feat(tracking): add TrackingProgress component for service order tracking

---
+++ app/rastreamento/components/TrackingProgress.tsx
@@ -0,0 +1,9 @@
+import { ServiceOrderTrackingResponse } from "@/services/types";
+
+export function TrackingProgress(props: ServiceOrderTrackingResponse) {
+  return (
+    <section>
+
+    </section>
+  )
+}
